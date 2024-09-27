194a59b9eb24db66f674d055e2cd0c102caf25cd
oksi-laza <153543859+oksi-laza@users.noreply.github.com>
2024-09-27 20:13:06 +0300

    Add files via upload
    Домашнее задание по теме "Хендлеры обработки сообщений".
    Задача "Бот поддержки (Начало)".

---
+++ module_13_2.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = ''  # получили в ТГ от BotFather
+bot = Bot(token=api)  # переменная бота - она будет хранить объект бота, 'token' будет равен вписанному ключу в 'api'
+dp = Dispatcher(bot, storage=MemoryStorage())  # создадим объект 'Dispatcher' и наш объект бота в качестве аргумента
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)  # запуск бота
